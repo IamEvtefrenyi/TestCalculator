1f3a774596aa6238da144b41ecee078f09c2aa4f
IamEvtefrenyi <ya24-03@yandex.ru>
2021-11-22 22:33:03 +0300

    Create MethodCalculator.java

---
+++ MethodCalculator.java
@@ -0,0 +1,22 @@
+package com.company.Calculator;
+
+public class MethodCalculator {
+    public static int calculated ( int num1, int num2, char oper){
+        int result = 0;
+        switch (oper) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
+        return result;
+    }
+}
